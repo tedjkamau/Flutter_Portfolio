7567a8fa71f0c3f60215bb78963c01280bda68a5
Teddy John Kamau Mbiri <Tedjkamau@gmail.com>
2022-05-10 23:09:46 +0100

    Did a box style container to practice

--- my_portfolio/lib/pages/home/home_page.dart
+++ my_portfolio/lib/pages/home/home_page.dart
@@ -7,7 +7,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(child: Text("Hello World")),
+      body: Center(
+          child: Container(
+        width: 300,
+        height: 100,
+        //Add equal padding to all sides,
+        padding: const EdgeInsets.all(25.0),
+        alignment: Alignment.bottomCenter,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(.15),
+              offset: Offset(0, 10),
+              blurRadius: 15,
+            )
+          ],
+          borderRadius: BorderRadius.circular(25),
+          border: Border.all(color: Colors.black, width: 2),
+        ),
+        child: const Text("Welcome :) My name is Ted"),
+      )),
     );
   }
 }
