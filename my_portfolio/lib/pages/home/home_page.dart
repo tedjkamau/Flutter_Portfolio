98ed514d9330b5fbef2e91010334a803c274e2d2
Teddy John Kamau Mbiri <Tedjkamau@gmail.com>
2022-05-10 21:59:55 +0100

    creating home screen

---
+++ my_portfolio/lib/pages/home/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+//Create Homepage class as a stateless widget
+class Homepage extends StatelessWidget{
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
