6a2d8c253642b8a4e77e4c19afad7c5fbe9f2db3
Teddy John Kamau Mbiri <Tedjkamau@gmail.com>
2022-05-11 20:20:31 +0100

    Learnt how to extract a widget as well was able to create a stateless widget by myself.

--- my_portfolio/lib/pages/home/home_page.dart
+++ my_portfolio/lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/widgets/cards/intro_card.dart';
 
 //Create HomePage which will be called to the main dart
 class HomePage extends StatelessWidget {
@@ -8,32 +9,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-            child: Padding(
-      //Add equal padding to all sides,
-      padding: const EdgeInsets.all(25.0),
-      child: Container(
-          padding: const EdgeInsets.all(25.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(.15),
-                offset: const Offset(0, 15),
-                blurRadius: 25,
-              )
-            ],
-            borderRadius: BorderRadius.circular(25),
-            border: Border.all(color: Colors.black, width: 2),
-          ),
-          child: Text(
-            "Welcome :) My name is Ted",
-            textAlign: TextAlign.center, //Text Alignment
-            style: TextStyle(
-                color: Colors.grey.shade600, //Add color to the text
-                fontSize: 20, //Font Size
-                fontWeight: FontWeight.bold //Font Weight
-                ), //Text style
-          )),
-    )));
+      child: IntroCard(
+        text: "High",
+      ),
+    ));
   }
-}
+} //A
