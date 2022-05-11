89c06943f39318b7dd41c543b4b5b14e794dac42
Teddy John Kamau Mbiri <Tedjkamau@gmail.com>
2022-05-11 20:01:47 +0100

    Learn structuring, container and padding, text manipulation

--- my_portfolio/lib/pages/home/home_page.dart
+++ my_portfolio/lib/pages/home/home_page.dart
@@ -7,33 +7,33 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Container(
-        width: 300,
-        height: 150,
-        //Add equal padding to all sides,
-        padding: const EdgeInsets.all(25.0),
-        alignment: Alignment.bottomCenter,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(.15),
-              offset: Offset(0, 10),
-              blurRadius: 15,
-            )
-          ],
-          borderRadius: BorderRadius.circular(25),
-          border: Border.all(color: Colors.black, width: 2),
-        ),
-        child: Text("Welcome :) My name is Ted",
-        textAlign: TextAlign.center,//Text Alignment
-        style:TextStyle(color: Colors.grey.shade900,//Add color to the text
-                        fontSize: 20,//Font Size
-                        fontWeight: FontWeight.bold//Font Weight
-                        ),//Text style
-      )),
-    )
-    );
+        body: Center(
+            child: Padding(
+      //Add equal padding to all sides,
+      padding: const EdgeInsets.all(25.0),
+      child: Container(
+          padding: const EdgeInsets.all(25.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(.15),
+                offset: const Offset(0, 15),
+                blurRadius: 25,
+              )
+            ],
+            borderRadius: BorderRadius.circular(25),
+            border: Border.all(color: Colors.black, width: 2),
+          ),
+          child: Text(
+            "Welcome :) My name is Ted",
+            textAlign: TextAlign.center, //Text Alignment
+            style: TextStyle(
+                color: Colors.grey.shade600, //Add color to the text
+                fontSize: 20, //Font Size
+                fontWeight: FontWeight.bold //Font Weight
+                ), //Text style
+          )),
+    )));
   }
 }
