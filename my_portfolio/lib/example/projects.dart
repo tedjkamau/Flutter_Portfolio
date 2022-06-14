a1016280b84d401570d8426a4b9589b8ff8fe9c5
Teddy John Kamau Mbiri <Tedjkamau@gmail.com>
2022-06-14 19:26:08 +0100

    Set new images for the project

--- my_portfolio/lib/example/projects.dart
+++ my_portfolio/lib/example/projects.dart
@@ -7,12 +7,12 @@ List<Project> projects = [
       description:
           "This is my final project where the use of deep learning techniques of 4 models were used to test effectiveness of classification",
       imageUrl:
-          "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"),
+          "https://upload.wikimedia.org/wikipedia/commons/a/a1/Normal_posteroanterior_%28PA%29_chest_radiograph_%28X-ray%29.jpg"),
   Project(
       name: "Chat Bot",
       year: 2022,
       description:
           "This project was to help build my deep learning skills and the creation of a chat bot(to be updated)",
       imageUrl:
-          "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"),
+          "https://media.istockphoto.com/vectors/robot-icon-bot-sign-design-chatbot-symbol-concept-voice-support-bot-vector-id1073043572?k=20&m=1073043572&s=612x612&w=0&h=WPpV4zW2oxJWS9nseA0IDFthj2IXsE7dbGBrPTGQNOI="),
 ];
